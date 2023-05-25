adde5db49fa70ae555835e9024414b356ebd34c7
ChoLam Wong (S3796627) <S3796627@student.rmit.edu.au>
2023-05-25 09:47:26 +0800

    fixed About us and dedication

---
+++ src/components/Dedication.js
@@ -0,0 +1,102 @@
+import  React, { useRef, useEffect } from 'react'
+import '../styles/dedication.css'
+import AnewoodLogo from '../blob/anewood_logo.png'
+
+
+function Dedication() {
+      
+    const about_main_ref = useRef()
+    
+    useEffect(()=>{
+        document.title = 'Anemoia Studios New Web | dedication';
+    // eslint-disable-next-line
+    }, [])
+
+    function scrollToHorizontal(event) {
+        const amount = event.deltaY
+        about_main_ref.current.scrollLeft += amount
+    }
+    return (
+        <div className='dedication-main' ref={about_main_ref} onWheel={scrollToHorizontal}>
+         <div className='dedication'>
+         <div className='about'>
+                <img className='main-img' src={AnewoodLogo} alt='anewood logo' />
+                <span className='summary'>
+                    Here are some interns that dedicated to projects of Anemoia
+                </span>
+            </div> 
+
+
+            <div className='team-container'>
+            <span className='title'>The Anemoia website</span>
+            <div className= 'intern-container '>
+            <div className='intern-container  intern'>
+                <img className='intern-container  photo'  alt='PlaceHolder' />
+                <span>PlaceHolder</span>
+            </div>
+            <div className='intern-container  intern'>
+            <img className='intern-container  photo'  alt='PlaceHolder' />
+            <span>PlaceHolder</span>
+            </div>
+            <div className='intern-container intern'>
+            <img className='intern-container photo'  alt='PlaceHolder' />
+            <span>PlaceHolder</span>
+            </div>
+            </div>
+            <div className= 'intern-container '>
+            <div className='intern-container intern'>
+                <img className='intern-container  photo'  alt='PlaceHolder' />
+                <span>PlaceHolder</span>
+            </div>
+            <div className='intern-container intern'>
+            <img className='intern-container photo'  alt='PlaceHolder' />
+            <span>PlaceHolder</span>
+            </div>
+            <div className='intern-container intern'>
+            <img className='intern-container photo'  alt='PlaceHolder' />
+            <span>PlaceHolder</span>
+            </div>
+            </div>
+            </div>
+
+            
+            <div className='team-container'>
+                <span className='title'>The app</span>
+            <div className= 'intern-container'>
+            <div className='intern-container intern'>
+                <img className='intern-container  photo'  alt='PlaceHolder' />
+                <span>PlaceHolder</span>
+            </div>
+            <div className='intern-container intern'>
+            <img className='intern-container photo'  alt='PlaceHolder' />
+            <span>PlaceHolder</span>
+            </div>
+            <div className='intern-container intern'>
+            <img className='intern-container photo'  alt='PlaceHolder' />
+            <span>PlaceHolder</span>
+            </div>
+            </div>
+            <div className= 'intern-container'>
+            <div className='intern-container intern'>
+                <img className='intern-container photo'  alt='PlaceHolder' />
+                <span>PlaceHolder</span>
+            </div>
+            <div className='intern-container intern'>
+            <img className='intern-container photo'  alt='PlaceHolder' />
+            <span>PlaceHolder</span>
+            </div>
+            <div className='intern-container intern'>
+            <img className='intern-container photo'  alt='PlaceHolder' />
+            <span>PlaceHolder</span>
+            </div>
+            </div>
+            </div>
+
+
+            </div>
+            
+        </div>
+    )
+}
+
+export default Dedication;
