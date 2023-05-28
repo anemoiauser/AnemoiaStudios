6b6793424469cd04b5f9ef45f6f9ad14fea8ea5d
cbh778899 <cbh778899@outlook.com>
2023-05-28 19:00:17 +1000

    add urls

--- src/settings.js
+++ src/settings.js
@@ -1,25 +1,25 @@
 export const project_titles = {
     studio: [
-        {name: 'Writers Studio', link: ''},
-        {name: 'Publishing House', link: ''},
-        {name: 'BANI Studio', link: ''},
-        {name: 'EMOIA Entertainment', link: ''},
-        {name: 'Film Studio', link: ''},
-        {name: 'Broadcasting Studio', link: ''},
-        {name: 'Music Studio', link: ''},
-        {name: 'Audition Center', link: ''},
-        {name: 'Manufacturing Center', link: ''}
+        {name: 'Writers Studio', link: '/projects/anemoia/WritersStudio'},
+        {name: 'Publishing House', link: '/projects/anemoia/'},
+        {name: 'BANI Studio', link: '/projects/anemoia/AnimationStudio'},
+        {name: 'EMOIA Entertainment', link: '/projects/anemoia/AdultEntertainment'},
+        {name: 'Film Studio', link: '/projects/anemoia/FilmStudio'},
+        {name: 'Broadcasting Studio', link: '/projects/anemoia/BroadcastingStudio'},
+        {name: 'Music Studio', link: '/projects/anemoia/MusicStudio'},
+        {name: 'Audition Center', link: '/projects/anemoia/AuditionCenter'},
+        {name: 'Manufacturing Center', link: '/projects/anemoia/'}
     ],
     anewood: [
-        {name:'Warehouse', link: ''},
-        {name:'Distribution Center', link: ''},
-        {name:'ANÉ Theatre', link: ''},
-        {name:'Podcast', link: ''},
-        {name:'Lobby', link: ''},
-        {name:'Warming Suites', link: ''},
-        {name:'Corporate Lounge', link: ''},
-        {name:'MOIA Cafe', link: ''},
-        {name:'Anewood Arena', link: ''}
+        {name:'Warehouse', link: '/projects/anewood/Warehouse'},
+        {name:'Distribution Center', link: '/projects/anewood/'},
+        {name:'ANÉ Theatre', link: '/projects/anewood/Theatre'},
+        {name:'Podcast', link: '/projects/anewood/PodcastInternetBasedEntertainmentStudio'},
+        {name:'Lobby', link: '/projects/anewood/CorporateLobby'},
+        {name:'Warming Suites', link: '/projects/anewood/CorporateSleepingArea'},
+        {name:'Corporate Lounge', link: '/projects/anewood/CorporateLounge'},
+        {name:'MOIA Cafe', link: '/projects/anewood/Cafeteria'},
+        {name:'Anewood Arena', link: '/projects/anewood/'}
     ],
     outside: [
         {name: 'Anewood Production', link: '/tour/anewood'},
