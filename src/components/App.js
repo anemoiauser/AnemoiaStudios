06afae85f0790b78ffe86030a5b2366d271bfab5
cbh778899 <cbh778899@outlook.com>
2023-05-28 16:06:54 +1000

    merge all projects to a single component file

--- src/components/App.js
+++ src/components/App.js
@@ -8,34 +8,10 @@ import Design from "./Design";
 import AboutUs from "./AboutUs";
 import Tour from "./Tour";
 import Contact from "./Contact";
-import AnemoiaProjects from "./projects/AnemoiaProjects";
-import AnewoodProjects from "./projects/AnewoodProjects";
 import Dedication from "./Dedication";
-import AnemoiaProjectAnimationStudio1 from "./projects/AnemoiaProjectAnimationStudio1";
-import AnemoiaProjectAnimationStudio4 from "./projects/AnemoiaProjectAnimationStudio4";
-import AnemoiaProjectAnimationStudio5 from "./projects/AnemoiaProjectAnimationStudio5";
-import AnemoiaProjectAuditionCenter1 from "./projects/AnemoiaProjectAuditionCenter1";
-import AnemoiaProjectAuditionCenter6 from "./projects/AnemoiaProjectAuditionCenter6";
-import AnemoiaProjectAuditionCenter7 from "./projects/AnemoiaProjectAuditionCenter7";
-import AnemoiaProjectAuditionCenter8 from "./projects/AnemoiaProjectAuditionCenter8";
-import AnemoiaProjectBroadcastingStudio1 from "./projects/AnemoiaProjectBroadcastingStudio1";
-import AnemoiaProjectMusicStudio1 from "./projects/AnemoiaProjectMusicStudio1";
-import AnemoiaProjectMusicStudio4 from "./projects/AnemoiaProjectMusicStudio4";
-import AnemoiaProjectAdultEntertainment1 from "./projects/AnemoiaProjectAdultEntertainment1";
-import AnemoiaProjectAdultEntertainment4 from "./projects/AnemoiaProjectAdultEntertainment4";
-import AnemoiaProjectFilmStudio2 from "./projects/AnemoiaProjectFilmStudio2";
-import AnemoiaProjectFilmStudio4 from "./projects/AnemoiaProjectFilmStudio4";
-import AnemoiaProjectFilmStudio5 from "./projects/AnemoiaProjectFilmStudio5";
-import AnemoiaProjectFilmStudio6 from "./projects/AnemoiaProjectFilmStudio6";
-import AnewoodProjectPodcast from "./projects/AnewoodProjectPodcast";
-import AnewoodProjectLounge from "./projects/AnewoodProjectLounge";
-import AnewoodProjectLobby from "./projects/AnewoodProjectLobby";
-import AnewoodProjectCafe from "./projects/AnewoodProjectCafe";
-import AnewoodProjectSuite from "./projects/AnewoodProjectSuite";
-import AnewoodProjectTheatre from "./projects/AnewoodProjectTheatre";
-import AnewoodProjectWarehouse from "./projects/AnewoodProjectWarehouse";
 import AnemoiaThemeSong from '../blob/Anemoia Theme Song Instrumentals.wav'
-import AnemoiaProjectWritersStudio6 from "./projects/AnemoiaProjectWritersStudio6";
+import AnewoodProductions from "./projects/AnewoodProductions";
+import SingleProject from "./projects/SingleProject";
 
 const TRACKING_ID = ""; // TRACKING_ID
 
@@ -74,33 +50,9 @@ function App() {
                     <Route path="outside" element={<Tour scene='outside' frames={7} />} />
                 </Route>
                 <Route path="/projects">
-                    <Route path="anemoia" element={<AnemoiaProjects scene='anemoia' />} />
-                    <Route path="anewood" element={<AnewoodProjects scene='anewood' />} />
+                    <Route path=":project" element={<AnewoodProductions />} />
+                    <Route path=":project/:name" element={<SingleProject />} />
                 </Route>
-                <Route path="/projects/AdultEntertainment1" element={<AnemoiaProjectAdultEntertainment1 />} />
-                <Route path="/projects/AdultEntertainment4" element={<AnemoiaProjectAdultEntertainment4 />} />
-                <Route path="/projects/AnimationStudio1" element={<AnemoiaProjectAnimationStudio1 />} />
-                <Route path="/projects/AnimationStudio4" element={<AnemoiaProjectAnimationStudio4 />} />
-                <Route path="/projects/AnimationStudio5" element={<AnemoiaProjectAnimationStudio5 />} />
-                <Route path="/projects/FilmStudio2" element={<AnemoiaProjectFilmStudio2 />} />
-                <Route path="/projects/FilmStudio4" element={<AnemoiaProjectFilmStudio4 />} />
-                <Route path="/projects/FilmStudio5" element={<AnemoiaProjectFilmStudio5 />} />
-                <Route path="/projects/FilmStudio6" element={<AnemoiaProjectFilmStudio6 />} />
-                <Route path="/projects/AuditionCenter1" element={<AnemoiaProjectAuditionCenter1 />} />
-                <Route path="/projects/AuditionCenter6" element={<AnemoiaProjectAuditionCenter6 />} />
-                <Route path="/projects/AuditionCenter7" element={<AnemoiaProjectAuditionCenter7 />} />
-                <Route path="/projects/AuditionCenter8" element={<AnemoiaProjectAuditionCenter8 />} />
-                <Route path="/projects/BroadcastingStudio1" element={<AnemoiaProjectBroadcastingStudio1 />} />
-                <Route path="/projects/MusicStudio1" element={<AnemoiaProjectMusicStudio1 />} />
-                <Route path="/projects/MusicStudio4" element={<AnemoiaProjectMusicStudio4 />} />
-                <Route path="/projects/WritersStudio6" element={<AnemoiaProjectWritersStudio6 />} />
-                <Route path="/projects/AnewoodProjectPodcast" element={<AnewoodProjectPodcast />} />
-                <Route path="/projects/AnewoodProjectLounge" element={<AnewoodProjectLounge />} />
-                <Route path="/projects/AnewoodProjectLobby" element={<AnewoodProjectLobby />} />
-                <Route path="/projects/AnewoodProjectCafe" element={<AnewoodProjectCafe />} />
-                <Route path="/projects/AnewoodProjectSuite" element={<AnewoodProjectSuite />} />
-                <Route path="/projects/AnewoodProjectTheatre" element={<AnewoodProjectTheatre />} />
-                <Route path="/projects/AnewoodProjectWarehouse" element={<AnewoodProjectWarehouse />} />
                 <Route path="/dedication" element={<Dedication />}/>
                 <Route path="*" element={<Error />} />
             </Routes>
---
+++ src/components/projects/SingleProject.js
@@ -0,0 +1,41 @@
+import  { React, useEffect, useRef } from 'react'
+import { Link, useParams } from 'react-router-dom'
+import '../../styles/anemoia_project.css'
+import AnemoiaLogo from '../../blob/anemoia_logo.png'
+import BackButton from  '../../blob/projects/bkbut.png'
+import { default as all_projects } from './projects';
+import Error from '../Error'
+
+function SingleProject() {
+    const { project, name } = useParams()
+    const this_project = useRef(all_projects[project][name])
+
+    useEffect(()=>{
+        document.title = 'Anemoia Studios - Project';
+    // eslint-disable-next-line
+    }, [])
+
+    return (
+        <>{this_project.current ? 
+        <>
+            <img className='logo' alt='Anemoia studios logo' src={AnemoiaLogo} />
+            <div className='anemoia_project'>
+                <span className='title'>{this_project.current.title}</span>
+                {   this_project.current.quote ?
+                    <span className='quote'>{this_project.current.quote}</span>:
+                    <></>
+                }
+                <Link to={`/projects/${project}`} className='projects-back-button'>
+                    <img alt='Projects back button' src={BackButton} />
+                    Go Back
+                </Link>
+                {this_project.current.imgs.map((e, i) => {
+                    return <img className='project-img' key={`project-img-${i}`} alt={this_project.title} src={e}/>
+                })}
+            </div>
+        </> : <Error />}
+        </>
+    )
+}
+
+export default SingleProject
