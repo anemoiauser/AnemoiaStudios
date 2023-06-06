43c9765ca70fc30ca5614dd02983e6084dfa4296
ChoLam Wong (S3796627) <S3796627@student.rmit.edu.au>
2023-06-06 15:32:22 +0800

    updated about us

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -3,7 +3,14 @@ import '../styles/about_us.css'
 import AnewoodLogo from '../blob/anewood_logo.png'
 import FemaleFounderLogo from '../blob/FemaleFounderLogo.png'
 import NewMediaFilmFestival from '../blob/NewMediaFilmFestival.png'
-import CEOPhoto from '../blob/CEO copy.png'
+import HonorableMentionTheMacoprojectFilmFestival2021 from '../blob/Honorable Mention The Macoproject Film Festival 2021.png'
+import OfficalSelectionTheMacoprojectFilmFestival2021 from '../blob/Offical Selection The Macoproject Film Festival 2021.png'
+import HonorableMentionDiamondEye from '../blob/Honorable Mention Diamond Eye.png'
+import OfficalSelectionDiamondEye from '../blob/Offical Selection Diamond Eye.png'
+import WinnerDiamondEye from '../blob/Winner Diamond Eye.png'
+import Emergingscreenwriters from '../blob/Emerging screen writers.png'
+import CEOPhoto from '../blob/Bej’a A. Christmas.png'
+import CIOPhoto from '../blob/Kāya Hampton.png'
 import Wanting from '../blob/wanting.png'
 
 function AboutUs() {
@@ -32,14 +39,24 @@ function AboutUs() {
                 </span>
                 <img className='affiliation' src={FemaleFounderLogo} alt='Female Founder Logo' />
                 <img className='affiliation' src={NewMediaFilmFestival} alt='New Media Film Festival' />
+<<<<<<< Updated upstream
             </div>
+=======
+                <img className='affiliation' src={HonorableMentionTheMacoprojectFilmFestival2021} alt='The Macoproject Film Festival 2021' />
+                <img className='affiliation' src={OfficalSelectionTheMacoprojectFilmFestival2021} alt='The Macoproject Film Festival 2021' />
+                <img className='affiliation' src={HonorableMentionDiamondEye} alt='Diamond Eye' />
+                <img className='affiliation' src={OfficalSelectionDiamondEye} alt='Diamond Eye' />
+                <img className='affiliation' src={WinnerDiamondEye} alt='Diamond Eye' />
+                <img className='affiliation' src={Emergingscreenwriters} alt='Emergingscreenwriters' />
+            </div> 
+>>>>>>> Stashed changes
             <div className='people-container'>
             <div className='about-block people'>
                 <img className='photo' src={CEOPhoto} alt='CEO' />
                 <span className='name'>Bejá A.Christmas</span>
                 <span>Founder & CEO</span>
                 <span style={{marginTop: '20px'}}>
-                Bejá has over twelve years of experience in the entertainment industry and has worked in different roles and capacities. Being a black woman in America, she has experienced adversity and has also discovered that minority/ethnic groups lack representation in the film and television industry. Using her experience, passion, and history, she is building Anemoia Inc. as a dream to represent the true culture of Black Americans and other POC in America.
+                With over twelve years of experience in the entertainment industry. I have worked in different roles and capacities. Being a black woman in America, I have experienced adversity and has also discovered that minority/ethnic groups lack representation in the film and television industry. Using my experience, passion, and history, I am building Anemoia to represent the true culture of Black Americans and other American People of Color on a global scale.
                 </span>
             </div>
             <div className='about-block people'>
@@ -71,11 +88,11 @@ function AboutUs() {
             </div>
             <div className='people-container'>
             <div className='about-block people'>
-                <img className='photo' src={Wanting} alt='Wanting Position' />
-                <span className='name'>OPEN POSITION</span>
+                <img className='photo' src={CIOPhoto} alt='Wanting Position' />
+                <span className='name'>Kāya Hampton</span>
                 <span>CIO</span>
                 <span style={{marginTop: '20px'}}>
-                Seeking a qualified individual(s) with a background in operations and infrastructure, specifically regarding production studios & corporate offices. Most importantly an individual(s) that wishes to help build Anemoia Inc., into a safe, profitable, and successful corporate entity for Black people and other marginalized minorities in North America. (Executive Experience preferred but not required)”
+                I’m a Certified ServiceNow Administrator and Certified Scrum Master from Alexandria, VA. I’m currently working in the project management field. Besides being an IT professional, I’m a Wellness Practitioner and Creative. 
                 </span>
             </div>
             </div>
---
+++ src/components/Dedication.js
@@ -0,0 +1,95 @@
+import  React, { useRef } from 'react'
+import '../styles/dedication.css'
+import AnewoodLogo from '../blob/anewood_logo.png'
+import YenNhiDoan from '../blob/interns/Yen Nhi Doan.png'
+import JiashengLiang from '../blob/interns/Jiasheng Liang.png'
+import HoangLinhNguyen from '../blob/interns/Hoang Linh Nguyen.png'
+import SaiVishnuAnudeepKadiyala from '../blob/interns/Sai Vishnu Anudeep Kadiyala.png'
+import MinNgo from '../blob/interns/Min Ngo.png'
+import DarshanPrakashbhaiPanchal from '../blob/interns/Darshan Prakashbhai Panchal.png'
+import SarthakVats from '../blob/interns/Sarthak Vats.png'
+
+function Dedication() {
+      
+    const about_main_ref = useRef()
+
+    function scrollToHorizontal(event) {
+        const amount = event.deltaY
+        about_main_ref.current.scrollLeft += amount
+    }
+    return (
+        <div className='dedication-main' ref={about_main_ref} onWheel={scrollToHorizontal}>
+         <div className='dedication'>
+         <div className='about'>
+                <img className='main-img' src={AnewoodLogo} alt='anewood logo' />
+                <span className='summary'>
+                    Here are some interns that dedicated to projects of Anemoia
+                </span>
+            </div> 
+
+
+            <div className='team-container'>
+            <div className= 'intern-container '>
+            <div className='intern-container  intern'>
+                <img className='intern-container  photo' src ={YenNhiDoan} alt='Yen Nhi Doan' />
+                <h3>Yen Nhi Doan</h3>
+                <span>I’m a tech enthusiast with a passion for frontend and mobile development! I made UI Design & Mobile App Developing contributions to the “Partnership Publishing Anewood Publishing” App</span>
+            </div>
+            <div className='intern-container  intern'>
+            <img className='intern-container  photo' src = {HoangLinhNguyen} alt='Hoang Linh Nguyen' />
+            <h3>Hoang Linh Nguyen</h3>
+            <span>I am currently contributing to the Partner Publishing Anewood Publishing App as a mobile developer. I am responsible for building the app interface, integrate API to manage user data, and optimize the app performance. I am a Vietnamese developer who is passionate about technology especially in mobile development sector.</span>
+            </div>
+            <div className='intern-container intern'>
+            <img className='intern-container photo' src = {JiashengLiang}  alt='Jiasheng Liang' />
+            <h3>Jiasheng Liang</h3>
+            <span name ='intern'>I created a financial analysis for Anemoia Studios.
+            <br />BS in SUNY at Albany 
+            <br />MS in SUNY Binghamton</span>
+            </div>
+            </div>
+            <div className= 'intern-container'>
+            <div className='intern-container intern'>
+                <img className='intern-container photo' src = {SaiVishnuAnudeepKadiyala} alt='Sai Vishnu Anudeep Kadiyala' />
+                <h3>Sai Vishnu Anudeep Kadiyala</h3>
+                <span><b>Software Engineer</b><br />
+                <br />As a self-motivated graduate student pursuing a Master's degree in Computer and Information Sciences at the University at Albany, State University of New York, I am actively seeking an internship position in software engineering.
+                <br />During my internship with Anemoia Studios I took lead and designed and developed a user-friendly workplace review cross-platform app known as “The Red Book”.</span>
+            </div>
+            <div className='intern-container intern'>
+            <img className='intern-container photo' src = {MinNgo} alt='Min Ngo' />
+            <h3>Min Ngo</h3>
+            <span><b>Motion & Graphic Designer</b><br/>
+            <br/>A multi-talented motion designer, graphic designer, and illustrator with a passion for all things magical and cute. An award-winning student at the RTA School of Media, I love experimenting with different styles to enhance visual storytelling. 
+            With experience designing for numerous nonprofits and startups, I especially love to intertwine community care with art! I contributed storyboard and animated trailer concepts for Anemoia Studios.</span>
+            </div>
+            <div className='intern-container intern'>
+            <img className='intern-container photo' src = {DarshanPrakashbhaiPanchal} alt='Darshan Prakashbhai Panchal' />
+            <h3>Darshan Prakashbhai Panchal</h3>
+            <span><b>Full Stack Developer</b><br/>
+            <br />During my internship, I had the opportunity to work on a diverse range of technologies, including Express.js, Expo, Node.js, SQLite, and React Native. My responsibilities encompassed both backend and frontend development. 
+            <br />I had an amazing experience during my internship at Anemoia Studios, working on the “Mind Ya Wellness” App that has a big impact on the company's social impact. 
+            <br />I'm proud of the results I achieved and how my work positively affected the company. This experience has motivated me to continue using technology to make a difference in the software development field.</span>
+            </div>
+            </div>
+            <div style= {{width: "30%"}} className='intern-container intern'>
+            <img className='intern-container photo' src = {SarthakVats} alt='Sarthak Vats' />
+            <h3>Sarthak Vats</h3>
+            <span><b>Software Engineer</b><br/>
+            <br />I am a student with a master's degree in software engineering, equipped with a strong foundation in software development and engineering principles. With a passion for technology and a drive for excellence, I am committed to making a positive impact in the field of software engineering.
+            <br />During my time at Anemoia Studios, I ran point and collaborated closely with the android team to develop an iOS app that provided a secure platform known as “Mind Ya Wellness” for clients to connect with therapists.
+            </span>
+            </div>
+
+            </div>
+
+            
+
+
+            </div>
+            
+        </div>
+    )
+}
+
+export default Dedication;
