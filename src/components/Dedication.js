c9571ad8d4c67b6950824a53c7b67106c9c81f5b
ChoLam Wong (S3796627) <S3796627@student.rmit.edu.au>
2023-06-06 14:34:13 +0800

    updated dedication

--- src/components/Dedication.js
+++ src/components/Dedication.js
@@ -1,7 +1,9 @@
 import  React, { useRef } from 'react'
 import '../styles/dedication.css'
 import AnewoodLogo from '../blob/anewood_logo.png'
-
+import YenNhiDoan from '../blob/interns/Yen Nhi Doan.png'
+import JiashengLiang from '../blob/interns/Jiasheng Liang.png'
+import HoangLinhNguyen from '../blob/interns/Hoang Linh Nguyen.png'
 
 function Dedication() {
       
@@ -23,69 +25,28 @@ function Dedication() {
 
 
             <div className='team-container'>
-            <span className='title'>The Anemoia website</span>
             <div className= 'intern-container '>
             <div className='intern-container  intern'>
-                <img className='intern-container  photo'  alt='PlaceHolder' />
-                <span>PlaceHolder</span>
+                <img className='intern-container  photo' src ={YenNhiDoan} alt='Yen Nhi Doan' />
+                <h3>Yen Nhi Doan</h3>
+                <span>I’m a tech enthusiast with a passion for frontend and mobile development! I made UI Design & Mobile App Developing contributions to the “Partnership Publishing Anewood Publishing” App</span>
             </div>
             <div className='intern-container  intern'>
-            <img className='intern-container  photo'  alt='PlaceHolder' />
-            <span>PlaceHolder</span>
+            <img className='intern-container  photo' src = {HoangLinhNguyen} alt='Hoang Linh Nguyen' />
+            <h3>Hoang Linh Nguyen</h3>
+            <span>I am currently contributing to the Partner Publishing Anewood Publishing App as a mobile developer. I am responsible for building the app interface, integrate API to manage user data, and optimize the app performance. I am a Vietnamese developer who is passionate about technology especially in mobile development sector.</span>
             </div>
             <div className='intern-container intern'>
-            <img className='intern-container photo'  alt='PlaceHolder' />
-            <span>PlaceHolder</span>
-            </div>
-            </div>
-            <div className= 'intern-container '>
-            <div className='intern-container intern'>
-                <img className='intern-container  photo'  alt='PlaceHolder' />
-                <span>PlaceHolder</span>
-            </div>
-            <div className='intern-container intern'>
-            <img className='intern-container photo'  alt='PlaceHolder' />
-            <span>PlaceHolder</span>
-            </div>
-            <div className='intern-container intern'>
-            <img className='intern-container photo'  alt='PlaceHolder' />
-            <span>PlaceHolder</span>
+            <img className='intern-container photo' src = {JiashengLiang}  alt='Jiasheng Liang' />
+            <h3>Jiasheng Liang</h3>
+            <span name ='intern'>I created a financial analysis for Anemoia Studios.
+            <br />BS in SUNY at Albany 
+            <br />MS in SUNY Binghamton</span>
             </div>
             </div>
             </div>
 
             
-            <div className='team-container'>
-                <span className='title'>The app</span>
-            <div className= 'intern-container'>
-            <div className='intern-container intern'>
-                <img className='intern-container  photo'  alt='PlaceHolder' />
-                <span>PlaceHolder</span>
-            </div>
-            <div className='intern-container intern'>
-            <img className='intern-container photo'  alt='PlaceHolder' />
-            <span>PlaceHolder</span>
-            </div>
-            <div className='intern-container intern'>
-            <img className='intern-container photo'  alt='PlaceHolder' />
-            <span>PlaceHolder</span>
-            </div>
-            </div>
-            <div className= 'intern-container'>
-            <div className='intern-container intern'>
-                <img className='intern-container photo'  alt='PlaceHolder' />
-                <span>PlaceHolder</span>
-            </div>
-            <div className='intern-container intern'>
-            <img className='intern-container photo'  alt='PlaceHolder' />
-            <span>PlaceHolder</span>
-            </div>
-            <div className='intern-container intern'>
-            <img className='intern-container photo'  alt='PlaceHolder' />
-            <span>PlaceHolder</span>
-            </div>
-            </div>
-            </div>
 
 
             </div>
